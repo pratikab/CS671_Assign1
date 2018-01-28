a15ff98a25ce5c3cad95cdc9c7fdaf04d692580f
Pratik Bhangale <pratik@localhost.localdomain>
2018-01-28 22:42:27 +0530

    1b done

--- 1b.py
+++ 1b.py
@@ -3,15 +3,12 @@ import fileinput
 import re
 def my_replace(match):
 	match = match.group()
-	temp = match.split("\'")
-	temp2 = '\''.join(temp[1:len(temp)-1])
-	temp3 = temp[0]+'\"'+temp2+'\"'+temp[len(temp)-1]
-	return temp3
-out_file = open('1a_output.txt', 'w')
+	return "<s> "+match+" </s>"
+out_file = open('1b_output.txt', 'w')
 in_file = open('test.txt', 'r')
 data = in_file.read()
 data2 = data.split('\n\n')
-regex = r'\s\'([^\']|(\w\')|(\s\'(.|\s)*?\'(\s|;)))*?[?.!,-]\'\s'
+regex = r'[\'"]?[A-Z]([^A-Z]|[A-Z](.|\s){2,2}?){2,}?([\.!?]|--(\n|\'\n))[\'"]?(?=(\s)*?[\'"]?([A-Z]|$))'
 out_data = ''
 for i in range(len(data2)):
 	feed = '\n'+data2[i]+'\n'
